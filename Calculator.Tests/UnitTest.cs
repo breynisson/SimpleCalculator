915e5f26a97a60187689de7206caec0e0d4a4e9c
breynisson <bjorgvin.reynisson@gmail.com>
2019-04-12 14:50:12 +0000

    Adding very basic unit-tests.

---
+++ Calculator.Tests/UnitTest.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.Pages;
+
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class UnitTestCalc
+    {
+        [TestMethod]
+        public void TestCalcAddTwoNumbers()
+        {
+            IndexModel indexModel = new IndexModel {firstNumber = 7, secondNumber = 3};
+            indexModel.OnPost();
+            Assert.AreEqual(10, indexModel.Result);
+        }
+
+        [TestMethod]
+        public void TestCalcFailingConditon()
+        {
+            IndexModel indexModel = new IndexModel {firstNumber = 7, secondNumber = 3};
+            indexModel.OnPost();
+            Assert.AreNotEqual(11, indexModel.Result);
+        }
+    }
+}
