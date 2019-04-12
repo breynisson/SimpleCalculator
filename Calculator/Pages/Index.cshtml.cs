e4056c8b851717950b789abf3a2a7fbe654f2e14
breynisson <bjorgvin.reynisson@gmail.com>
2019-04-12 09:33:11 +0000

    Add basic adding functionality.

--- Calculator/Pages/Index.cshtml.cs
+++ Calculator/Pages/Index.cshtml.cs
@@ -9,9 +9,22 @@ namespace Calculator.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public float firstNumber { get; set; }
+        [BindProperty]
+        public float secondNumber { get; set; }
+        [BindProperty]
+        public float Result { get; set; }
+
+        
         public void OnGet()
         {
+            
+        }
 
+        public void OnPost()
+        {
+            Result = firstNumber + secondNumber;
         }
     }
 }
